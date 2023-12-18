0ba9d72529edf5c2362d42d034292f102b6b06f7
Barbara <b.olejniczak@suora.com>
2023-12-18 09:08:56 +0100

    feat: create and implement Todo component

---
+++ src/components/TodoInput.tsx
@@ -0,0 +1,6 @@
+import { Input } from "@nextui-org/react";
+import { FunctionComponent } from "react";
+
+export const TodoInput: FunctionComponent = () => {
+    return <Input style={{border: '5px solid black' }}/>;
+};
