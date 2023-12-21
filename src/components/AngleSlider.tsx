9cfe2b396b9624df52a25c5e1e1301fa7b27bf50
Barbara <b.olejniczak@suora.com>
2023-12-21 17:38:18 +0100

    feat: add props to angleSlider

--- src/components/AngleSlider.tsx
+++ src/components/AngleSlider.tsx
@@ -1,6 +1,15 @@
 import { FC } from "react";
-import {Slider} from "@nextui-org/react";
+import { Slider } from "@nextui-org/react";
 
 export const AngleSlider: FC = () => {
-    return <Slider />
-}
+  return (
+    <Slider
+      label="Angle"
+      step={0.01}
+      maxValue={1}
+      minValue={0}
+      defaultValue={0.4}
+      className="max-w-md"
+    />
+  );
+};
