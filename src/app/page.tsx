48062ec31a5eab5227b1b600da194099aaa5a619
Barbara <b.olejniczak@suora.com>
2024-01-16 14:07:47 +0100

    feat: add method for calculate of refracted light

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@
 import { AngleSlider } from "@/components/AngleSlider";
 import { LightBeam } from "@/components/LightBeam";
 import { TodoInput } from "@/components/TodoInput";
+import { Button } from "@nextui-org/react";
 import { transform } from "framer-motion";
+import { Philosopher } from "next/font/google";
 import { root } from "postcss";
+import { pid } from "process";
 import { useState } from "react";
 import { styled } from "styled-components";
 // const ReflectionBeam = styled.div`
@@ -16,17 +19,35 @@ import { styled } from "styled-components";
 
 export default function Home() {
   const [angleValue, setAnglaValue] = useState<number>(0);
-
+  const [refractedAngle, setRefractedAngle] = useState(0);
+  const getRefractedAngle = () => {
+    const n1Air = 1.000277;
+    const n2Diamond = 2.417;
+    const valueOfRefractedAngle = (n1Air * Math.sin(angleValue)) / n2Diamond;
+    const value = (Math.asin(valueOfRefractedAngle) * 180) / Math.PI;
+    return setRefractedAngle(value);
+  };
+  const changeAngle = () => {
+    setAnglaValue
+    const n1Air = 1.000277;
+    const n2Diamond = 2.417;
+    const valueOfRefractedAngle = (n1Air * Math.sin(angleValue)) / n2Diamond;
+    setRefractedAngle(Math.asin(valueOfRefractedAngle));
+  };
   return (
     <main>
       <div className="angle-simulator">
         <TodoInput valueInput={angleValue} />
+        <Button onClick={changeAngle}>x</Button>
         <AngleSlider
           angleValueSlider={angleValue}
-          onAngleChange={setAnglaValue}
+          onAngleChange={changeAngle}
         />
         {/* <ReflectionBeam></ReflectionBeam> */}
-        <LightBeam lightBeamAngle={angleValue} />
+        <LightBeam
+          lightBeamAngle={angleValue}
+          refractedBeamAngle={refractedAngle}
+        />
       </div>
     </main>
   );
