ee6a834a9e130ed1e66331a4b15ab773f3c2eece
Barbara <b.olejniczak@suora.com>
2024-01-16 14:06:57 +0100

    refactor: change type

--- src/components/AngleSlider.tsx
+++ src/components/AngleSlider.tsx
@@ -4,7 +4,7 @@ import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 
 interface AngleSliderProps {
-  angleValueSlider: any;
+  angleValueSlider: number;
   onAngleChange: (newValue: any) => void;
 }
 
