1388ba3f34830f46dc837f7bc2606df0a47462e9
Barbara <b.olejniczak@suora.com>
2024-01-16 14:06:39 +0100

    feat: add light beams

--- src/components/LightBeam.tsx
+++ src/components/LightBeam.tsx
@@ -1,46 +1,66 @@
+import { relative } from "path";
 import { FC } from "react";
 
 interface LightBeamProps {
   lightBeamAngle: number;
+  refractedBeamAngle: number;
 }
 
-export const LightBeam: FC<LightBeamProps> = ({ lightBeamAngle }) => {
+export const LightBeam: FC<LightBeamProps> = ({ lightBeamAngle, refractedBeamAngle }) => {
   return (
-    <section style={{ display: "flex", flexDirection: "row" }}>
-      <div
-        style={{
-          width: "1px",
-          height: "150px",
-          color: "red",
-          border: "solid red",
-          transformOrigin: "bottom",
-          transform: `rotate(${lightBeamAngle}deg)`,
-        }}
-      >
-        <div style={{ padding: "10px", transform: "rotate(90deg)" }}>
-          incidentRay
-        </div>
-      </div>
-      <div
-        style={{
-          width: "1px",
-          height: "150px",
-          color: "red",
-          border: "solid red",
-          transformOrigin: "bottom",
-          transform: `rotate(${-lightBeamAngle}deg)`,
-        }}
-      >
+    <div style={{ display: "flex", flexDirection: "column", gap: "0" }}>
+      <div style={{ display: "flex", flexDirection: "row" }}>
         <div
           style={{
-            padding: "10px",
-            transform: "rotate(90deg)",
-            scale: "1",
+            width: "1px",
+            height: "150px",
+            color: "red",
+            border: "solid red",
+            transformOrigin: "bottom",
+            transform: `rotate(${lightBeamAngle}deg)`,
           }}
         >
-          refelctedRay
+          <div style={{ padding: "10px", transform: "rotate(90deg)" }}>
+            incidentRay
+          </div>
+        </div>
+        <div
+          style={{ width: "1px", height: "150px", border: "solid blue" }}
+        ></div>
+
+        <div
+          style={{
+            width: "1px",
+            height: "150px",
+            color: "red",
+            border: "solid red",
+            transformOrigin: "bottom",
+            transform: `rotate(${-lightBeamAngle}deg)`,
+          }}
+        >
+          <div
+            style={{
+              padding: "10px",
+              transform: "rotate(90deg)",
+              scale: "1",
+            }}
+          >
+            refelctedRay
+          </div>
         </div>
       </div>
-    </section>
+
+      <div
+        style={{
+          width: "1px",
+          height: "150px",
+          border: "solid hotpink",
+          transformOrigin: "top",
+          transform: `rotate(${refractedBeamAngle}deg)`,
+          position: "relative",
+          left: " 3px",
+        }}
+      ></div>
+    </div>
   );
 };
